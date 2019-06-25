daf819a46bb24bd2e55ff5cefacd34515d0edcb8
tolga.kamci <tolga.kamci@BK-LAB204-IS020>
2019-06-25 11:45:54 +0300

    template eklendi.

---
+++ Fakebook/Fakebook.CoreUI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fakebook.CoreUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fakebook.CoreUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Fakebook/Fakebook.CoreUI/Startup.cs
+++ Fakebook/Fakebook.CoreUI/Startup.cs
@@ -53,7 +53,7 @@ namespace Fakebook.CoreUI
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
